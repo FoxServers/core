b48747fd9bb69aeab4be499dea57f50d4c36c321
Michael Fuchs <89425629+TheMiniSandwich@users.noreply.github.com>
2024-11-18 19:21:40 -0500

    add -y to apt installs

--- apt-install.sh
+++ apt-install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 if apt; then
     echo "Updating apt packages..."
-    sudo apt update && sudo apt upgrade
+    sudo apt update -y
+    sudo apt upgrade -y
     echo "Installing samba..."
-    sudo apt install samba
+    sudo apt install samba -y
     echo "Installing python..."
-    sudo apt install python3
+    sudo apt install python3 -y
     echo "Installing docker-compose..."
-    sudo apt install docker-compose
+    sudo apt install docker-compose -y
     echo "Done!"
 fi
