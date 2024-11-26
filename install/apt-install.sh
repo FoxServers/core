e74378d37c62bc18f1fcfc974dda122bb219a9cf
Michael Fuchs <89425629+TheMiniSandwich@users.noreply.github.com>
2024-11-26 15:11:51 -0500

    Created first verion of install script for Ubuntu, will rework repos so components can be installed seperately

--- install/apt-install.sh
+++ install/apt-install.sh
@@ -7,6 +7,8 @@ if apt; then
     sudo apt install samba -y
     echo "Installing python..."
     sudo apt install python3 -y
+    sudo apt install python3-pip -y
+    sudo apt install python3-venv -y
     echo "Installing docker-compose..."
     sudo apt install docker-compose -y
     echo "Done!"
--- install/smb.sh
+++ install/smb.sh
@@ -2,11 +2,17 @@
 echo "Backing up smb config..."
 sudo cp -pf /etc/samba/smb.conf /etc/samba/smb.conf.bak
 echo "Creating server folder..."
-sudo mkdir -p /srv/server
-sudo chmod a+rwx /srv/server
+sudo mkdir -p /srv/foxservers/
+sudo chmod a+rwx /srv/foxservers/
 echo "Editing smb config to include new folder..."
-if ! grep -F "[Server]" /etc/samba/smb.conf; then
-    echo "[Server]\n  path = /srv/server\n  browseable = yes\n  guest ok = no\n  writeable = yes\n  read only = no\n  valid users = @smb" >> /etc/samba/smb.conf
+if ! grep -F "[FoxServers]" /etc/samba/smb.conf; then
+    sudo /usr/bin/bash -c 'echo "[FoxServers]
+    path = /srv/foxservers
+    browseable = yes
+    guest ok = no
+    writeable = yes
+    read only = no
+    valid users = @smb" >> /etc/samba/smb.conf'
 fi
 echo "Updating firewall settings..."
 sudo ufw allow samba
@@ -19,7 +25,8 @@ USER="${USER:-$CURRENT_USER}"
 echo "User $USER was selected..."
 sudo smbpasswd -a "$USER"
 echo "Updating folder permissions..."
-sudo chmod -R g+rwx /srv/server
-sudo chown "$USER":smb /srv/server
+sudo chmod -R g+rwx /srv/foxservers/
+sudo chown "$USER":smb /srv/foxservers/
 echo "Restarting smb service..."
 sudo service smbd restart
+echo "Done!"
